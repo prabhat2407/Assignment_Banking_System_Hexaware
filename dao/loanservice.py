5bc9897daa3bc39e9d563df1c20ad52ccf56501a
Prabhat <prabhatgprai@gmail.com>
2023-12-19 15:38:56 +0530

    AssignmentHexaware

---
+++ dao/loanservice.py
@@ -0,0 +1,10 @@
+from entity.customer import Customer
+from exception.exceptions import LoanEligibilityException
+
+class LoanService:
+    @staticmethod
+    def check_loan_eligibility(customer: Customer):
+        if customer.credit_score > 700 and customer.annual_income >= 50000:
+            return True
+        else:
+            raise LoanEligibilityException("Loan eligibility criteria not met.")
